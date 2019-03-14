b894faaf5fabf4c79a4b96f223c15d7e6b92b2f7
Gabriel Consalter <consaltergabriel@gmail.com>
2019-03-13 22:01:19 -0300

    New app, JSON connect

---
+++ server.R
@@ -0,0 +1,31 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library("rjson")
+
+json_file <- "epl.json"
+json_data <- fromJSON(paste(readLines(json_file), collapse=""))
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$AC <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$AC + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
